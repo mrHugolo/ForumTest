1be2c8aabdb8195b8a39330701a3ea924282c0c7
tripleWdotcom <mespinoza.arias@gmail.com>
2022-01-12 20:00:40 +0100

    CSS theme test and frontend fixes
    - removed some unnecessary re-renders
    - User DOES NOT login automatically after creating an account
    - dark theme button added
    - chnaged <Link> to <useHistory>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,43 +1,48 @@
-import {useContext, useState } from "react";
-import {Link } from "react-router-dom";
-import {UserContext } from "../contexts/UserContext";
+import { useContext, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+import css from "../styles/index.module.css";
 
-export const Navbar = () => {
-  const { currentUser,setCurrentUser } = useContext(UserContext);
-  const [dark, setDark]= useState(false)
-
-  console.log("Current user atm: ",currentUser);
-
+export const Navbar = ({ changeTheme, dark }) => {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const history = useHistory();
 
 
-  const toggleTheme=()=>{
-    setDark(prev=>!prev)
-  }
+  //for dev purpose, remove when done
+  const memoUserlog = useMemo(() => {
+    console.log("Current user atm: ", currentUser);
+  }, [currentUser]);
+  memoUserlog;
 
+  const toggleTheme = () => {
+    changeTheme((prev) => !prev);
+  };
 
-  const logout=async()=>{
-    await fetch('/api/logout',{
-      method:'DELETE'
-    })
-    setCurrentUser('')
-    console.log("logged oout from session")
-    
-  }
+  const logout = async () => {
+    await fetch("/api/logout", {
+      method: "DELETE",
+    });
+    setCurrentUser("");
+    console.log("logged oout from session");
+  };
 
-
+  const goToPage = (e) => {
+    if (history.location.pathname == `${e.target.id}`) return; // Dont re-render if already on the same path
+    history.push(`${e.target.id}`);
+  };
 
   return (
-    <>
-      <Link to="/">Home</Link>
-      <button onClick={toggleTheme}>{dark ?"dark" : "light" } </button>
-      <h1>Navbar</h1>
-      <div>
+    <div className={css.navbar}>
+      <button onClick={toggleTheme}>{!dark ? "dark" : "light"} theme </button>
+      <div className={css.loginCreateWrapper}>
+        <h1 onClick={goToPage} id="/">Hidden Forum</h1>
+        <h4 onClick={goToPage} id="/register">Create account</h4>
         {currentUser ? (
-          <button onClick={logout}>Logout</button>
+          <h4 onClick={logout}>Logout</h4>
         ) : (
-          <Link to="/login">Login</Link>
+          <h4 onClick={goToPage} id="/login">Login</h4>
         )}
       </div>
-    </>
+    </div>
   );
 };
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,41 +1,46 @@
-import { useState, useContext } from "react";
-import { Link } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 export const Login = () => {
-  const { setCurrentUser } = useContext(UserContext);
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-
+  const { setCurrentUser, currentUser } = useContext(UserContext);
   const [badCredentials, setBadCredentials] = useState(false);
+  let history = useHistory();
 
+  useEffect(() => {
+    if (currentUser) history.push("/");
+  }, [currentUser]);
 
-  async function login(e) {
+  const gotoPage = () => {
+    history.push("/register");
+  };
+
+  const credentials = { username: "", password: "" };
+  const handleUsername = (e) => credentials.username = e.target.value;
+  const handlePassword = (e) => credentials.password = e.target.value;
+
+  async function login(e) { 
     e.preventDefault();
-
-    const credentials = {
-      username,
-      password,
-    };
-
     let res = await fetch("/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(credentials),
     });
-    
+
     let user = await res.json();
-    
+
     if (res.status == 401) {
       setBadCredentials(true);
-    }
-    else if (res.status == 200) {
+      document.getElementById("username").value=""
+      document.getElementById("password").value ="";
+      setTimeout(()=>setBadCredentials(false),3000)
+    } else if (res.status == 200) {
       setCurrentUser(user);
     }
   }
 
+
+  //all classes are from tailwind remove later
   return (
     <>
       <h1>LOGIN</h1>
@@ -44,19 +49,17 @@ export const Login = () => {
         <input type="hidden" name="remember" defaultValue="true" />
         <div className="rounded-md shadow-sm -space-y-px">
           <div>
-            <label htmlFor="username-address" className="sr-only">
+            <label htmlFor="username" className="sr-only">
               username
             </label>
             <input
-              id="username-address"
+              id="username"
               name="username"
               autoComplete="username"
               required
               className="appearance-none rounded-none font-myPtext text-xl relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-myGr-dark focus:border-myGr-dark focus:z-10 sm:text-sm"
               placeholder="username"
-              onChange={(e) => {
-                setUsername(e.target.value), setBadCredentials(false);
-              }}
+              onChange={handleUsername}
             />
           </div>
           <div>
@@ -71,27 +74,21 @@ export const Login = () => {
               required
               className="appearance-none rounded-none font-myPtext text-xl relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-myGr-dark focus:border-myGr-dark focus:z-10 sm:text-sm"
               placeholder="Password"
-              onChange={(e) => {
-                setPassword(e.target.value), setBadCredentials(false);
-              }}
+              onChange={handlePassword}
             />
           </div>
         </div>
         <div className="text-xs text-center">
           {badCredentials && (
-            <div className="text-myRe -mt-2 mb-2 font-medium">
-              {" "}
+            <div style={{color:"red"}}>
               Bad Credentials{" "}
             </div>
           )}
-          <Link to="/register">
-            Register Account
-          </Link>
+          <h5 onClick={gotoPage}>Create account</h5>
         </div>
         <div>
           <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-myGr-light focus:bg-myGr-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-myGr-dark">
-            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-            </span>
+            <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
             Log in
           </button>
         </div>
