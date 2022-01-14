d1d03098cace8911f6192c75b5718c4a10be3361
Hugo Granath <72066319+mrHugolo@users.noreply.github.com>
2022-01-14 15:52:01 +0100

    profile backend, frontend

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,12 +2,16 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import pcss from "../styles/profile.module.css"
+import css from "../styles/index.module.css"
 import { Fetch } from "../utils/fetch";
+import { BsFillPencilFill } from "react-icons/bs"
 
 export const Profile = () => {
   const {currentUser} = useContext(UserContext)
   const {userName} = useParams()
   const [user, setUser] = useState({})
+  const [updater, setUpdater] = useState('')
+  const [isEdit, setIsEdit] = useState(false)
 
   useEffect( async () => {
     let info = (await Fetch(`rest/profile/${userName}`)).response
@@ -15,7 +19,7 @@ export const Profile = () => {
       name: info[0].username,
       description: info[0].description,
       groups: info[0].names.split('ᴥ'),
-      isMyProfile: false,
+      isMyProfile: currentUser?.username == userName,
       comments: []
     }
     for(let i = 0; i < info.length; i++) {
@@ -26,18 +30,45 @@ export const Profile = () => {
       })
     }
     setUser(u)
-  }, [])
+  }, [updater])
 
-  useEffect(()=> {
-    if(currentUser?.id) {
-      let bol = currentUser.username == userName
-      setUser(p => ({ ...p, isMyProfile: bol}))
-    } 
+  useEffect(() => {
+    if(currentUser?.id) setUpdater('update')
   }, [currentUser])
+
+  const handleEdit = async (e) => {
+    if(e.key != 'Enter' || e.shiftKey) return
+    setIsEdit(false)
+    let res = (await Fetch(`rest/editDescription`, {
+      method: "PATCH",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({description: e.target.value}),
+    })).response
+    if(res.status == 200) {
+      setUser(p => ({ ...p, description: e.target.value }))
+    } 
+  }
 
   return (
     <div className={pcss.container}>
-      <h1 onClick={() => console.log(user)}>{userName}</h1>
+      <h1>{user.name}</h1>
+      <div className={`${css.borderBottom} ${css.w100}`}>
+        {user.isMyProfile && (
+          <div onClick={() => setIsEdit(p => !p)}><BsFillPencilFill /></div>
+        )}
+        {isEdit ? (
+          <textarea onKeyPress={handleEdit} defaultValue={user.description}></textarea>
+        ) : (
+          <div className={pcss.left}> {user.description}</div>
+        )}
+      </div>
+
+      <h2>Groups:</h2>
+      {user.groups ? user.groups.map(g => (
+        <div key={`profileGroup-${g}`} className={css.groupCard}>
+          {g}
+        </div>
+      )) : (<div>No groups yet!</div>)}
     </div>
   );
 };
